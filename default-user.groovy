5ed496e21b20743e07b5f40c38d58bca4508d016
sivakumar-droid <64016348+sivakumar-droid@users.noreply.github.com>
2021-02-13 19:29:35 +0530

    Update default-user.groovy

--- default-user.groovy
+++ default-user.groovy
@@ -12,6 +12,9 @@ if(!(jenkins.getAuthorizationStrategy() instanceof GlobalMatrixAuthorizationStra
 
 // create new Jenkins user account
 // username & password from environment variables
+
+// this env variable comes from dockerfile with environment variable JENKINS_USER and JENKINS_PASS
+
 def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
 user.save()
 jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, env.JENKINS_USER)
